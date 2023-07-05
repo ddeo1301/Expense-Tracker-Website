e0eee8e84e0ef6d44540be4f3081f091e9fc93d0
ddeo1301 <divyanshudeo1301@gmail.com>
2023-07-06 00:17:23 +0530

    cicddeployment

--- ExpenseTrackerFrontend/Login/login.js
+++ ExpenseTrackerFrontend/Login/login.js
@@ -27,33 +27,3 @@ function forgotpassword() {
 }
 
 
-
-// function login(e) {
-//     e.preventDefault();
-//     console.log(e.target.name);
-//     const form = new FormData(e.target);
-
-//     const loginDetails = {
-//         email: form.get("email"),
-//         password: form.get("password")
-
-//     }
-
-//     console.log(loginDetails)
-
-//     axios.post('http://18.233.6.175:3000/user/login',loginDetails).then(response => {
-//         if(response.status === 200){
-//             localStorage.setItem('token', response.data.token);
-//             localStorage.setItem('userDetails', JSON.stringify(response.data.user))
-//             window.location.href = "../ExpenseTracker/expense.html" // change the page on successful login
-//         } else {
-//             throw new Error('Failed to login')
-//         }
-//     }).catch(err => {
-//         document.body.innerHTML += `<div style="color:red;">${err} <div>`;
-//     })
-// }
-
-// function forgotpassword() {
-//     window.location.href = "../ForgotPassword/forgotpassword.html"
-// }
